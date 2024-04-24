3cc174e7c6076addeac6b03d8531680e67a1c951
Shawn Merana <shawnmerana@gmail.com>
2024-04-24 11:27:58 -0700

    New descriptions

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,15 @@ import React from "react";
 const HomePage = () => {
     return (
         <div>
-            <h1>Welcome page</h1>
+            <div>
+                <h1>Welcome page</h1>
+                <p>This section is to mention any recent findings with vunerabilities and stuff</p>
+            </div>
+            <div>
+                <h3>Extra tidbits go here</h3>
+                <p>New device that has been introduced to the market</p>
+                <p>Links to the reviews of professionals related to each device</p>
+            </div>
         </div>
     );
 }
--- src/pages/Quizzlet.js
+++ src/pages/Quizzlet.js
@@ -4,6 +4,10 @@ const Quizzer = () => {
     return (
         <div>
             <h1>Page containing different quizzes</h1>
+            <p>
+                Research more on how they were to adapt that
+                Java program to create a QA section from a given input.
+            </p>
         </div>
     );
 }
