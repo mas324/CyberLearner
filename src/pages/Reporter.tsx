3c61aba5def898765cb8c3fa9307a865f31cc793
Shawn Merana <shawnmerana@gmail.com>
2024-04-29 11:22:38 -0700

    New type definitions and psudocode
    Defined CISA type
    Defined Quizzlet type

--- src/pages/Reporter.tsx
+++ src/pages/Reporter.tsx
@@ -1,11 +1,26 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 
+type CISA = {
+    cveID: string;
+    vendorProject: string;
+    product: string;
+    vulnerabilityName: string;
+    dateAdded: string;
+    shortDescription: string;
+    requiredAction: string;
+    dueDate: string;
+    knownRansomwareCampaignUse: string;
+    notes: string;
+}
+
 function Items({ id, date, ransomware, desc, vendor }) {
     return (
         <div>
             <h3>{vendor}</h3>
-            {desc}
+            <p>Known as of {date}</p>
+            <p>{desc}</p>
+            <p>Used for ransomware: {ransomware}</p>
         </div>
     )
 }
@@ -13,7 +28,8 @@ function Items({ id, date, ransomware, desc, vendor }) {
 const Report = () => {
     // TODO: create a fetch for data
     // TODO: create a link to the ai responder
-    const [data, setData] = useState([]);
+
+    const [data, setData] = useState(Array<CISA>);
 
     useEffect(() => {
         if (data.length > 0) {
@@ -29,9 +45,14 @@ const Report = () => {
             setData(res.data.vulnerabilities);
         }).catch((err) => {
             console.error(err);
-        })
+        }).finally(() => {
+            let sort = data.slice();
+            sort.sort((a, b) =>
+                Date.parse(a.dateAdded) - Date.parse(b.dateAdded)
+            );
+            setData(sort);
+        });
     }, [data]);
-
 
     return (
         <div>
@@ -43,12 +64,14 @@ const Report = () => {
                         key={index}
                         vendor={value.vendorProject}
                         desc={value.shortDescription}
-                    />
+                        id={value.cveID}
+                        date={value.dateAdded}
+                        ransomware={value.knownRansomwareCampaignUse === "Known"} />
                 })
             ) : (null)}
             <h2>Other reports</h2>
         </div>
-    );
+    )
 }
 
 export default Report;
